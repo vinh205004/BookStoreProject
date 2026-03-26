472713f85f74b5526aa02ce3e97c5637a798f5b4
LaiVinh <vinhlai005@gmail.com>
2026-03-26 15:44:57 +0700

    First commit: Khoi tao du an

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Website Bán Sách - React + TS + Tailwind cực mượt!
+      </h1>
+    </div>
+  )
+}
+
+export default App
